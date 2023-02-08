adfcc5421a3c6406226bedbd3c4077602cd1d87f
Matthew Sherohman <mattshero40@gmail.com>
2023-02-08 00:38:36 -0600

    added the dashboard plugin

--- lua/shorent/config.lua
+++ lua/shorent/config.lua
@@ -2,4 +2,5 @@ vim.wo.number = true
 vim.opt.colorcolumn = '80'
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.shiftwidth = 4
+vim.opt.shiftwidth = 4
+vim.wo.wrap = false
--- lua/shorent/plugins.lua
+++ lua/shorent/plugins.lua
@@ -1,8 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
@@ -22,13 +17,17 @@ return require('packer').startup(function(use)
 		end
 	})
 
+	-- Dashboard
+	use {'glepnir/dashboard-nvim'}
+	use {'nvim-tree/nvim-web-devicons'}
+
 	-- Treesitter
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('nvim-treesitter/playground')
-	
+
 	-- UndoTree
 	use('mbbill/undotree')
-		
+
 	-- Git
 	use('tpope/vim-fugitive')
 
