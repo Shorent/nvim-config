5a5313b0cdc84293b530cb3716da57f1eda758fd
Matthew Sherohman <mattshero40@gmail.com>
2023-02-22 20:56:57 -0600

    added terminal keymaps

--- lua/shorent/core/keymaps.lua
+++ lua/shorent/core/keymaps.lua
@@ -9,10 +9,13 @@ function keymaps.telescope()
     vim.keymap.set('n', '<leader>ff', tb.find_files, {})
     vim.keymap.set('n', '<leader>fg', tb.git_files, {})
     vim.keymap.set('n', '<leader>ft', function()
-        tb.grep_string({ 
-            search = vim.fn.input("Grep > ") 
+        tb.grep_string({
+            search = vim.fn.input("Grep > ")
         });
     end)
+    vim.keymap.set('n', '<leader>tt', ':top split term://zsh <CR>', {})
+    vim.keymap.set('n', '<leader>tb', ':below split term://zsh <CR>', {})
+    vim.keymap.set('n', '<leader>tl', ':left vsplit term://zsh <CR>', {})
 end
 
 -- vim-fugitive
